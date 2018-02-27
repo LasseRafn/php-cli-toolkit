6e0968829ef7f649acda66bbf813c77d2d37efa0
Lasse Rafn <lasserafn@gmail.com>
2018-02-27 17:07:09 +0100

    Colorize output

---
+++ src/Color.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace LasseRafn\CliToolkit;
+
+class Color
+{
+	const DEFAULT      = '\e[0m';
+	const WHITE        = '\e[1;37m';
+	const BLACK        = '\e[0;30m';
+	const BLUE         = '\e[0;34m';
+	const LIGHT_BLUE   = '\e[1;34m';
+	const GREEN        = '\e[0;32m';
+	const LIGHT_GREEN  = '\e[1;32m';
+	const CYAN         = '\e[0;36m';
+	const LIGHT_CYAN   = '\e[1;36m';
+	const RED          = '\e[0;31m';
+	const LIGHT_RED    = '\e[1;31m';
+	const PURPLE       = '\e[0;35m';
+	const LIGHT_PURPLE = '\e[1;35m';
+	const BROWN        = '\e[0;33m';
+	const YELLOW       = '\e[1;33m';
+	const GRAY         = '\e[0;30m';
+	const LIGHT_GRAY   = '\e[0;37m';
+
+
+	public static function default( $text = '' ) { return self::DEFAULT . $text . self::DEFAULT; }
+
+	public static function white( $text = '' ) { return self::WHITE . $text . self::DEFAULT; }
+
+	public static function black( $text = '' ) { return self::BLACK . $text . self::DEFAULT; }
+
+	public static function blue( $text = '' ) { return self::BLUE . $text . self::DEFAULT; }
+
+	public static function light_blue( $text = '' ) { return self::LIGHT_BLUE . $text . self::DEFAULT; }
+
+	public static function green( $text = '' ) { return self::GREEN . $text . self::DEFAULT; }
+
+	public static function light_green( $text = '' ) { return self::LIGHT_GREEN . $text . self::DEFAULT; }
+
+	public static function cyan( $text = '' ) { return self::CYAN . $text . self::DEFAULT; }
+
+	public static function light_cyan( $text = '' ) { return self::LIGHT_CYAN . $text . self::DEFAULT; }
+
+	public static function red( $text = '' ) { return self::RED . $text . self::DEFAULT; }
+
+	public static function light_red( $text = '' ) { return self::LIGHT_RED . $text . self::DEFAULT; }
+
+	public static function purple( $text = '' ) { return self::PURPLE . $text . self::DEFAULT; }
+
+	public static function light_purple( $text = '' ) { return self::LIGHT_PURPLE . $text . self::DEFAULT; }
+
+	public static function brown( $text = '' ) { return self::BROWN . $text . self::DEFAULT; }
+
+	public static function yellow( $text = '' ) { return self::YELLOW . $text . self::DEFAULT; }
+
+	public static function gray( $text = '' ) { return self::GRAY . $text . self::DEFAULT; }
+
+	public static function light_gray( $text = '' ) { return self::LIGHT_GRAY . $text . self::DEFAULT; }
+}
---
+++ src/helpers.php
@@ -0,0 +1,69 @@
+<?php
+
+if ( ! function_exists( 'cli_text_default' ) ) {
+	function cli_text_default( $text = '' ) { return \LasseRafn\CliToolkit\Color::default( $text ); }
+}
+
+if ( ! function_exists( 'cli_text_white' ) ) {
+	function cli_text_white( $text = '' ) { return \LasseRafn\CliToolkit\Color::white( $text ); }
+}
+
+if ( ! function_exists( 'cli_text_black' ) ) {
+	function cli_text_black( $text = '' ) { return \LasseRafn\CliToolkit\Color::black( $text ); }
+}
+
+if ( ! function_exists( 'cli_text_blue' ) ) {
+	function cli_text_blue( $text = '' ) { return \LasseRafn\CliToolkit\Color::blue( $text ); }
+}
+
+if ( ! function_exists( 'cli_text_light_blue' ) ) {
+	function cli_text_light_blue( $text = '' ) { return \LasseRafn\CliToolkit\Color::light_blue( $text ); }
+}
+
+if ( ! function_exists( 'cli_text_green' ) ) {
+	function cli_text_green( $text = '' ) { return \LasseRafn\CliToolkit\Color::green( $text ); }
+}
+
+if ( ! function_exists( 'cli_text_light_green' ) ) {
+	function cli_text_light_green( $text = '' ) { return \LasseRafn\CliToolkit\Color::light_green( $text ); }
+}
+
+if ( ! function_exists( 'cli_text_cyan' ) ) {
+	function cli_text_cyan( $text = '' ) { return \LasseRafn\CliToolkit\Color::cyan( $text ); }
+}
+
+if ( ! function_exists( 'cli_text_light_cyan' ) ) {
+	function cli_text_light_cyan( $text = '' ) { return \LasseRafn\CliToolkit\Color::light_cyan( $text ); }
+}
+
+if ( ! function_exists( 'cli_text_red' ) ) {
+	function cli_text_red( $text = '' ) { return \LasseRafn\CliToolkit\Color::red( $text ); }
+}
+
+if ( ! function_exists( 'cli_text_light_red' ) ) {
+	function cli_text_light_red( $text = '' ) { return \LasseRafn\CliToolkit\Color::light_red( $text ); }
+}
+
+if ( ! function_exists( 'cli_text_purple' ) ) {
+	function cli_text_purple( $text = '' ) { return \LasseRafn\CliToolkit\Color::purple( $text ); }
+}
+
+if ( ! function_exists( 'cli_text_light_purple' ) ) {
+	function cli_text_light_purple( $text = '' ) { return \LasseRafn\CliToolkit\Color::light_purple( $text ); }
+}
+
+if ( ! function_exists( 'cli_text_brown' ) ) {
+	function cli_text_brown( $text = '' ) { return \LasseRafn\CliToolkit\Color::brown( $text ); }
+}
+
+if ( ! function_exists( 'cli_text_yellow' ) ) {
+	function cli_text_yellow( $text = '' ) { return \LasseRafn\CliToolkit\Color::yellow( $text ); }
+}
+
+if ( ! function_exists( 'cli_text_gray' ) ) {
+	function cli_text_gray( $text = '' ) { return \LasseRafn\CliToolkit\Color::gray( $text ); }
+}
+
+if ( ! function_exists( 'cli_text_light_gray' ) ) {
+	function cli_text_light_gray( $text = '' ) { return \LasseRafn\CliToolkit\Color::light_gray( $text ); }
+}
